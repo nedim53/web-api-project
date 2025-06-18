21b2e92fd581351856c26d6e924d030e56d69b5b
nedim53 <zec.nedim13@gmail.com>
2025-06-18 20:35:20 +0200

    nist

--- app/src/main/java/com/example/web_api_project/data/remote/api/DatasetApi.kt
+++ app/src/main/java/com/example/web_api_project/data/remote/api/DatasetApi.kt
@@ -3,6 +3,7 @@ package com.example.web_api_project.data.remote.api
 import com.example.web_api_project.data.remote.dto.ApiResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
+import androidx.room.*
 
 interface DatasetApi {
     @GET("api/3/action/datastore_search")
